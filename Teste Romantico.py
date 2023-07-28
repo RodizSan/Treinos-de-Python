40bdd7b082d52aa337aea208aafc8069ab5c13f6
Rodrigo Conde Santos <110734047+RodizSan@users.noreply.github.com>
2023-07-27 21:27:42 -0300

    Add files via upload

---
+++ Teste Romantico.py
@@ -0,0 +1,8 @@
+nome1=input("Digite seu nome: ")
+nome2=input("Digite outro nome: ")
+if nome1=="nome_da_namorada" and nome2=="seu_nome":
+        print("Vocês super-combinam!")
+elif nome1=="seu_nome" and nome2=="nome_da_namorada":
+    print("Vocês super-combinam!")
+else:
+    print("Desculpe, vocês não combinam.")
